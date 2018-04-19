e24306400b2cb135045c1fe20e88ecd14b92671a
Saurabh Gandhi <saurabh@saurabh.local>
2018-04-20 00:30:43 +0530

    Data Migration

--- templates/migration_script.py
+++ templates/migration_script.py
@@ -2,7 +2,7 @@ import csv
 import django
 django.setup()
 from pages.models import Student
-with open('0_Final Registrations - All.csv','r') as csvfile:
+with open('FinalRegistrations.csv','r') as csvfile:
     spamreader =csv.DictReader(csvfile)
     for row in spamreader:
         Student.objects.create(name=row['Name'],programme=row['Programme'],allowed_entries=int(row['Count']),last_name=" ",middle_name=" ",remaining_entries=int(row['Count']))
